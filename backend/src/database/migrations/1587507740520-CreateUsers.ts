d242748dab4dba2fda911539b05958ceb1fa9937
Marcus Girolneto <marcus.girolneto@gmail.com>
2020-04-22 18:03:12 -0300

    Relacionamento nos models

--- backend/src/database/migrations/1587507740520-CreateUsers.ts
+++ backend/src/database/migrations/1587507740520-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1587507740520 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
