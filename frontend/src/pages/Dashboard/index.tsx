0f7a07682ec1cab29817a7ac7e35ed028efb1a98
Marcus Girolneto <marcus.girolneto@gmail.com>
2020-05-07 19:46:48 -0300

    Adicionando rotas e redirecionamento

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => (
+  <>
+    <h1>hsauahsauhsauh</h1>
+    <h1>shausaushu</h1>
+    <h1>shausaushu</h1>
+    <h1>shausaushu</h1>
+    <h1>shausaushu</h1>
+    <h1>shausaushu</h1>
+    <h1>shausaushu</h1>
+  </>
+);
+
+export default Dashboard;
