9e7c9b071425024f2ba15b6a132fa3cd1bc70968
Marcus Girolneto <marcus.girolneto@gmail.com>
2020-05-28 17:56:06 -0300

    Correções

--- backend/src/modules/users/services/ResetPasswordService.ts
+++ backend/src/modules/users/services/ResetPasswordService.ts
@@ -17,7 +17,7 @@ class ResetPasswordService {
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('MailProvider')
+    @inject('UserTokens')
     private userTokensRepository: IUserTokensRepository,
 
     @inject('HashProvider')
