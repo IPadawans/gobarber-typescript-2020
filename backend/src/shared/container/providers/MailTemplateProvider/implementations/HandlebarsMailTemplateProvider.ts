60b9704a4be0fc968ea248cb7c8a223015015cd9
Marcus Girolneto <marcus.girolneto@gmail.com>
2020-05-31 12:41:20 -0300

    Finalizando recover de senha

--- backend/src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
+++ backend/src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -1,14 +1,18 @@
 import handlebard from 'handlebars';
+import fs from 'fs';
 
 import IMailTemplateProvider from '../models/IModelTemplateProvider';
 import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 
 class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
   public async parse({
-    template,
+    file,
     variables,
   }: IParseMailTemplateDTO): Promise<string> {
-    const parseTemplate = handlebard.compile(template);
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+    const parseTemplate = handlebard.compile(templateFileContent);
 
     return parseTemplate(variables);
   }
