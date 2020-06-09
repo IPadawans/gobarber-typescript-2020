f798a3821b19217e0e846158ab6424242432cc2b
Marcus Girolneto <marcus.girolneto@gmail.com>
2020-06-09 17:42:54 -0300

    Lidando com disponibilidade de prestador

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -14,7 +15,7 @@ type IResponse = Array<{
 }>;
 
 @injectable()
-class ListProvidersService {
+class ListProvidersMonthAvailability {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
@@ -25,7 +26,7 @@ class ListProvidersService {
     year,
     month,
   }: IRequest): Promise<IResponse> {
-    const appointments = this.appointmentsRepository.findAllInMonthFromProvider(
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
       {
         provider_id,
         year,
@@ -33,14 +34,26 @@ class ListProvidersService {
       },
     );
 
-    console.log(appointments);
-    return [
-      {
-        day: 1,
-        available: false,
-      },
-    ];
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10, // 10 pois existe no maximo 10 agendamentos por dia
+      };
+    });
+
+    return availability;
   }
 }
 
-export default ListProvidersService;
+export default ListProvidersMonthAvailability;
