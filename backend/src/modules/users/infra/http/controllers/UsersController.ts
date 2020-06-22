8320e9b0514fdfb6db40f6b79f8737035db4d1b2
Marcus Girolneto <marcus.girolneto@gmail.com>
2020-06-22 13:49:43 -0300

    Aplicado class transformer

--- backend/src/modules/users/infra/http/controllers/UsersController.ts
+++ backend/src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
@@ -16,9 +17,7 @@ export default class UsersController {
       password,
     });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json({ user: classToClass(user) });
   }
 
   async update(request: Request, response: Response): Promise<Response> {
@@ -28,8 +27,6 @@ export default class UsersController {
       avatarFilename: request.file.filename,
     });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json({ user: classToClass(user) });
   }
 }
