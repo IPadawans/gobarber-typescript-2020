99e11b3ca692d9bfefe2287faf3834a68312b570
Marcus Girolneto <marcus.girolneto@gmail.com>
2020-06-24 11:36:21 -0300

    Adicionando cache

--- backend/src/modules/users/services/CreateUserService.ts
+++ backend/src/modules/users/services/CreateUserService.ts
@@ -1,9 +1,11 @@
 import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
-import User from '../infra/typeorm/entities/User';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+import User from '../infra/typeorm/entities/User';
 
 interface IRequest {
   name: string;
@@ -19,6 +21,9 @@ class CreateUserService {
 
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -35,6 +40,7 @@ class CreateUserService {
       password: hashedPassword,
     });
 
+    await this.cacheProvider.invalidatePrefix('providers-list');
     return user;
   }
 }
